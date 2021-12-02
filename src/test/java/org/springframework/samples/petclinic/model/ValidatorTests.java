f7beb3233ce438df4b59a3f4d40992ed51bafafb
Jasmine Smith <jasmine.smith@essvote.com>
2021-12-02 13:52:31 -0600

    break a test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("Yarmel");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
